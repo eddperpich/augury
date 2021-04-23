f8bbc623320562192ac30bab93e652f6cbb7b355
Edward Perpich <eddperpich@gmail.com>
2021-04-22 21:13:31 -0400

    one day i will make these fucking morons cry

--- src/test/java/com/eddperpich/augury/AuguryApplicationTests.java
+++ src/test/java/com/eddperpich/augury/AuguryApplicationTests.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AuguryApplicationTests {
 
 	@Test
-	@AutoConfigureTestDatabase
 	void contextLoads() {
 	}
 
